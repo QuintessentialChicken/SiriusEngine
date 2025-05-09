1ff6f00741a2d2e3fa0099b0c53f8a3f64571127
Leon Stier <Lms@gnadenweiler.de>
2025-05-09 14:06:56 +0200

    Removed unnecessary window proc

--- src/Sirius/Graphics/GfxDevice_Win32.cpp
+++ src/Sirius/Graphics/GfxDevice_Win32.cpp
@@ -42,38 +42,13 @@ LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
     return g_WindowProc(hWnd, msg, wParam, lParam);
 }
 
-LRESULT HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
-    // Get the pointer to the window and delegate the message to the HandleMsg function
-    // auto* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-    return HandleMsg(hWnd, msg, wParam, lParam);
-}
-
-LRESULT HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
-    if (msg == WM_NCCREATE) {
-        // Get the CREATESTRUCT we passed using lparam in CreateWindow() containing the initialization parameters of our window
-        const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
-        // Get a pointer to the window from the CREATESTRUCT
-        // auto* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
-        // Store the pointer in WINAPI-managed user data
-        // SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
-        // Set the window procedure in WINAPI-managed user data to HandleMsgThunk since we are leaving the setup
-        SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&HandleMsgThunk));
-        // SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(DefWindowProc));
-        // Delegate the current msg to HandleMsg
-        return HandleMsg(hWnd, msg, wParam, lParam);
-    }
-
-    // Delegate any message received before the WM_NCCREATE to the default window procedure
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
-
 HWND GfxDevice::CreateDeviceWindow() {
     if (!winClass) {
         hInstance = GetModuleHandle(nullptr);
         WNDCLASSEX wc{};
         wc.cbSize = sizeof(wc);
         wc.style = CS_OWNDC;
-        wc.lpfnWndProc = HandleMsgSetup;
+        wc.lpfnWndProc = HandleMsg;
         wc.cbClsExtra = 0;
         wc.cbWndExtra = 0;
         wc.hInstance = hInstance;
--- src/Sirius/Shaders/PixelShader.hlsl
+++ src/Sirius/Shaders/PixelShader.hlsl
@@ -3,5 +3,6 @@ cbuffer CBuf {
 };
 
 float4 main(uint tid : SV_PrimitiveID) : SV_Target {
+// Each triangle has a unique ID. Pick the color based on the triangle ID.
     return face_colors[tid / 2];
 }
