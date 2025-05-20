d16d00ddbf759deb85c7262ff9c4d88b9ea23024
Leon Stier <Lms@gnadenweiler.de>
2025-05-20 19:05:38 +0200

    Multiple Frames in flight

--- src/Sirius/Graphics/Vulkan/RenderApi_Vulkan.h
+++ src/Sirius/Graphics/Vulkan/RenderApi_Vulkan.h
@@ -103,7 +103,7 @@ private:
 
     void CreateCommandPool();
 
-    void CreateCommandBuffer();
+    void CreateCommandBuffers();
 
     void RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
 
@@ -128,10 +128,10 @@ private:
     VkPipeline graphicsPipeline = VK_NULL_HANDLE;
     std::vector<VkFramebuffer> swapChainFramebuffers;
     VkCommandPool commandPool = VK_NULL_HANDLE;
-    VkCommandBuffer commandBuffer = VK_NULL_HANDLE;
-    VkSemaphore imageAvailableSemaphore = VK_NULL_HANDLE;
-    VkSemaphore renderFinishedSemaphore = VK_NULL_HANDLE;
-    VkFence inFlightFence = VK_NULL_HANDLE;
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::vector<VkSemaphore> imageAvailableSemaphores;
+    std::vector<VkSemaphore> renderFinishedSemaphores;
+    std::vector<VkFence> inFlightFences;
 
 
 };
